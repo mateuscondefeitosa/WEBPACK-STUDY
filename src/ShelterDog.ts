eae8d80c9a9d90de4ef685ab3be0d9c689c17df0
Mateus Conde <devmateuscondefeitosa@gmail.com>
2022-07-07 10:08:28 -0300

    First commit

---
+++ src/ShelterDog.ts
@@ -0,0 +1,13 @@
+import Dog from "./Dog.js";
+
+export default class ShelterDog extends Dog {
+    constructor(
+        name: string, 
+        breed: string, 
+        age: number, 
+        public shelter: string
+    ) {
+        super(name, breed, age);
+    }
+    
+}
