d15622bb1ec5eb56c990abbe6b945bf74956d706
Mateus Conde <devmateuscondefeitosa@gmail.com>
2022-07-07 11:39:00 -0300

    Clean Webpack Plugin example

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const { CleanWebpackPlugin } = require ('clean-webpack-plugin');
 const path = require("path");
 
 module.exports = {
@@ -26,4 +27,5 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/dist",
   },
+  plugins: [new CleanWebpackPlugin()],
 };
