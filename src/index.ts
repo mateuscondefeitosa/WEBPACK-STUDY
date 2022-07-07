057f211db511428b1c074fa1714f3869b028a40f
Mateus Conde <devmateuscondefeitosa@gmail.com>
2022-07-07 11:35:26 -0300

    Webpack server

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
-import Dog from "./Dog.js";
-import ShelterDog from "./ShelterDog.js";
-import {add, multiply, divide} from "./utils.js"
+import Dog from "./Dog";
+import ShelterDog from "./ShelterDog";
+import { add, multiply, divide } from "./utils";
 
 console.log("TESTE1");
-
 
 const freya = new Dog("Freya", "aussie", 1);
 freya.bark();
 
-
-console.log(add(4,5));
-console.log(multiply(4,5));
-console.log(divide(4,5));
-
+console.log(add(4, 5));
+console.log(multiply(4, 5));
+console.log(divide(4, 5));
 
 const buffy = new ShelterDog("Buffy", "Pitbull", 5, "Desert Springs Shelter");
